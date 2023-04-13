d43cc7e7ec1e343ed8f70d321bc79fd189d546b9
Mathew Charm <mattcharmz@gmail.com>
2023-04-12 23:10:00 -0700

    added and modified

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541 
-  });
-  
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-
-  conn.on('end', () => {
-    console.log('disconnected from server');
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-  });
-    return conn;
-};
+const {connect} = require('./client')
 
 
 
