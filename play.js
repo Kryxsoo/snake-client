5767d6109e8fcd708012007fe8848fb2410c969b
Mathew Charm <mattcharmz@gmail.com>
2023-04-13 01:17:01 -0700

    2 functions

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 const net = require("net");
 const {connect} = require('./client')
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  const stdout = process.stdout;
+  if (key === '\u0003'){
+    process.exit();
+  }
+};
+
+
+
 
 
 console.log("Connecting ...");
